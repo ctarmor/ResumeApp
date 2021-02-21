240123e572b73109ff6a2f0d739a6b1904b38c2e
comtechy@live.com <comtechy@live.com>
2021-02-21 13:37:00 -0500

    Added FonsAwesome package.

--- src/components/TheFooter.tsx
+++ src/components/TheFooter.tsx
@@ -1,4 +1,22 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+//
+// https://react-bootstrap.github.io/layout/grid/
+//
+// Awesome Fonts Icons
+// https://fontawesome.com/how-to-use/on-the-web/using-with/react
+//
 
 export function TheFooter(props: any) {
-    return <div  style={{ }}>Footer</div>;
+
+    return <div style={{ display: 'block', textAlign: 'center' }}>
+        <hr/>
+        <FontAwesomeIcon icon={faHome} />&nbsp;<a href='/'>Home</a>
+
+        &nbsp;|&nbsp;
+        
+        <FontAwesomeIcon icon={faGithub} />&nbsp;<a href='https://github.com/ctarmor/ResumeApp' target='_new'>My GitHub ResumeApp</a>
+    </div>;
 }
